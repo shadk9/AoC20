bcba97ed789257471194660e21508a03e7998c4e
Shad Karim <shadk18@uia.no>
2020-12-19 20:02:37 +0100

    part 2

--- day9.py
+++ day9.py
@@ -7,8 +7,7 @@ with open('input_day9') as file:
 def day9(input, preambel):
     for i in range(len(input)):
 
-
-        preambel_list = [int(n) for n in input[i:(preambel+i)]]
+        preambel_list = [int(n) for n in input[i:(preambel + i)]]
         target = int(input[preambel + i])
         combinations_list = []
 
@@ -16,6 +15,30 @@ def day9(input, preambel):
             combinations_list.append(sum(num))
 
         if target not in combinations_list:
-            return (target)
+            return target
 
-print(day9(input, 25))
+
+#print(day9(input, 25))
+
+
+def day9_2(input, preambel_target):
+    repeats = 3
+    preambel = 3
+    target = day9(input, preambel_target)
+
+    while True:
+        for i in range(len(input)):
+            preambel_list = [int(n) for n in input[i:(preambel + i)]]
+            combinations_list = []
+
+            for num in itertools.combinations(preambel_list, repeats):
+                combinations_list.append(sum(num))
+
+            if target in combinations_list:
+                print(min(preambel_list) +  max(preambel_list))
+                return
+
+        preambel += 1
+        repeats += 1
+
+day9_2(input, 25)
